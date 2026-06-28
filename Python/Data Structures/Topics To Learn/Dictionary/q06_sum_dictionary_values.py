4133beaeee59b00a99f003d1a0c291a76bf5b0d2
Lakshya1003 <malviyalakshyaraj@gmail.com>
2026-06-28 11:51:44 +0530

    Data structures push

---
+++ Python/Data Structures/Topics To Learn/Dictionary/q06_sum_dictionary_values.py
@@ -0,0 +1,6 @@
+monthly_expenses = {"Rent": 1200, "Groceries": 450, "Utilities": 150, "Internet": 60}
+print("Here are the expenses:", monthly_expenses)
+
+total_amount_spent = sum(monthly_expenses.values())
+
+print("The total sum of all values is:", total_amount_spent)
